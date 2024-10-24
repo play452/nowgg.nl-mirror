86cf32632fcfea84d627c9a01e69a886d0e4765a
play452 <oscarlin@edu.schools.us.kg>
2024-10-24 12:12:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'mathsspot.com') {
+    if (req.headers.host === 'nowgg.nl') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
